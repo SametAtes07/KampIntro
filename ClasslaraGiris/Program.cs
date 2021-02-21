5b5e092973f123d043cc2921a1182c06c155a8b5
samet ateş <samettatess@gmail.com>
2021-02-22 02:29:01 +0300

    Add project files.

---
+++ ClasslaraGiris/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClasslaraGiris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Kurs kurs1 = new Kurs();
+
+            kurs1.KursunAdi = "C# ";
+            kurs1.KursunEgitmeni = "Samet Ateş ";
+            kurs1.İzlenmeOranı = 60;
+
+            Kurs kurs2 = new Kurs();
+
+            kurs2.KursunAdi = "Java Eğitimi ";
+            kurs2.KursunEgitmeni = "Ahmet Batman ";
+            kurs2.İzlenmeOranı = 31;
+
+            Kurs kurs3 = new Kurs();
+
+            kurs3.KursunAdi = "Valorant Nasıl oynanır ";
+            kurs3.KursunEgitmeni = "Marul ";
+            kurs3.İzlenmeOranı = 86;
+
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursunAdi + " -- " + kurs.KursunEgitmeni + "  %" + kurs.İzlenmeOranı);
+
+            }
+
+
+        }
+    }
+}
+class Kurs
+{
+    public string KursunAdi { get; set; }
+
+    public string KursunEgitmeni { get; set; }
+
+    public int İzlenmeOranı { get; set; }
+}
---
+++ Ödev1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Ödev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string KategoriVeri = "Kategori : Yazılımcı Olma Kampı";
+
+            bool SistemeGirisKontorl = true;
+
+            Console.WriteLine(KategoriVeri);
+            Console.WriteLine("   ");
+
+            if (SistemeGirisKontorl==true)
+            {
+                Console.WriteLine("Kurslarım  Tüm Kurslar   Kampa Hazırlık     ");
+
+                Console.WriteLine("    ");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Ol Butonu");
+
+                Console.WriteLine("    ");
+            }
+
+
+            EgitimKursu kurs1 = new EgitimKursu();
+
+            kurs1.KursunAdi = "Yazılım Geliştirici Yetiştirme Kampı ";
+            kurs1.KursunEgitmeni = "Engin Demiroğ ";
+            kurs1.KursİzlenmeOranı = 82;
+
+            EgitimKursu kurs2 = new EgitimKursu();
+
+            kurs2.KursunAdi = "Programlamaya Giriş İçin Temel Kurs ";
+            kurs2.KursunEgitmeni = "Engin Demiroğ ";
+            kurs2.KursİzlenmeOranı = 67;
+
+
+            EgitimKursu[] TümKurslar = new EgitimKursu[] {kurs1,kurs2 };
+
+            foreach (var kurs in TümKurslar)
+            {
+                Console.WriteLine(kurs.KursunAdi+"  -- "+kurs.KursunEgitmeni+"  %"+kurs.KursİzlenmeOranı);
+            }
+
+            
+
+
+        }
+    }
+}
+class EgitimKursu
+{
+    public string KursunAdi { get; set; }
+
+    public string KursunEgitmeni { get; set; }
+
+    public int KursİzlenmeOranı { get; set; }
+}
